875ccfb73ef59a9795bab3e308e4af5b03e2579f
yimikao <holayinkajr@gmail.com>
2022-05-11 00:44:04 +0100

    real from input stream and exex command

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 //keyboard is the standard input device(os.Stdin)
 
 func main() {
 
-  cmd_reader := bufio.NewReader() 
-  
+	cmd_reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+
+		input, err := cmd_reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		if err := execCmd(input); err != nil {
+			fmt.Fprint(os.Stderr, err)
+		}
+
+	}
+
+}
+
+func execCmd(cmd string) error {
+
+	cmd = strings.TrimSuffix(cmd, "\n")
+
+	command := exec.Command(cmd)
+
+	command.Stdin, command.Stderr = os.Stdin, os.Stderr
+
+	return command.Run()
+
 }
